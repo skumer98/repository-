78d6b44a456c77820aec4f47b16f957a204a3b9a
sara umer <135882691+skumer98@users.noreply.github.com>
2023-06-18 17:08:19 -0500

    Add files via upload

---
+++ CIS2348Summer2023/zybooklab1.py
@@ -0,0 +1,9 @@
+#Sara Umer 1999495
+
+#The assignment is to get an integer from input, and output that integer squared, ending with newline.
+
+userNum = int(input()) 
+
+userNumSquared = userNum*userNum # Bug here; fix it when instructed
+
+print(userNumSquared, end='\n') # Output formatting issue here; fix it when instructed
