863e909560dc64827c5baece2a0a831e4ac4fec1
sara umer <135882691+skumer98@users.noreply.github.com>
2023-06-18 17:08:03 -0500

    Delete Zybook lab 1.py

--- CIS2348Summer2023/Zybook lab 1.py
+++
@@ -1,9 +0,0 @@
-#Sara Umer 1999495
-
-#The assignment is to get an integer from input, and output that integer squared, ending with newline.
-
-userNum = int(input()) 
-
-userNumSquared = userNum*userNum # Bug here; fix it when instructed
-
-print(userNumSquared, end=' ') # Output formatting issue here; fix it when instructed
