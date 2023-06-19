94679454fa777eba2bd10a471809f6fda01918bd
sara umer <135882691+skumer98@users.noreply.github.com>
2023-06-18 22:20:00 -0500

    Add files via upload

---
+++ CIS2348Summer2023/zybooklab5.22.py
@@ -0,0 +1,41 @@
+#Sara Umer 199495
+
+#(1) Output a menu of automotive services and the corresponding cost of each service. (2 pts)
+
+print("Davy's auto shop services")
+print("Oil Change -- $35")
+print("Tire rotation -- $19")
+print("Car wash -- $7")
+print("Car wax -- $12")
+
+#(2) Prompt the user for two services from the menu. (2 pts)
+
+oil_change = ("Oil change") 
+tire_rotation = ("Tire rotation")
+car_wash = ("Car wash")
+car_wax = ("Car wax")
+
+print("Select first service:")
+service = input()
+
+print("Select second service:")
+service = input() 
+
+#(3) Output an invoice for the services selected. Output the cost for each service and the total cost. (3 pts)
+print("Davy's auto shop invoices")
+print("Service 1:",service,"$35")
+print("Service 2:",service,"$12")
+print("Total: $47")
+
+#(4) Extend the program to allow the user to enter a dash (-), which indicates no service. (3 pts)
+
+No_service = "-"
+
+
+print("Service 1:",service,"$19")
+print("Service 2:",service)
+
+
+
+
+
